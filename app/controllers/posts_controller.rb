32fd2c8503a913163d7346b6ae5c8ca478244a9f
cartagena <jcartagena93@gmail.com>
2019-03-03 20:27:36 -0500

    styling changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :company_id)
+      params.require(:post).permit(:title, :description, :company_id, tag_ids: [])
     end
 end
